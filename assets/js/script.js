5d47c5a382347a05cc6a01fe0963e14c69eb19b7
robindavidgames <robin.david.games@gmail.com>
2021-12-25 07:15:26 +0000

    Add placeholder functions to script.js.

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+/**
+ * Begins the game on page load.
+ */
+function runGame() {
+
+}
+
+/**
+ * Starts a new turn. Increments turn counter and triggers dice roll.
+ */
+function newTurn() {
+
+}
+
+/**
+ * Randomises the two dice values, between 1 and 6.
+ */
+function randomiseDice() {
+
+}
+
+/**
+ * Allows user to click on a dice and assign that value to a game space.
+ */
+function assignDice() {
+
+}
+
+/**
+ * Calculates the score of a row.
+ */
+function calculateRow() {
+
+}
+
+/**
+ * Calculates the score of a column.
+ */
+function calculateColumn() {
+
+}
+
+/**
+ * Calculates total game score.
+ */
+function calculateScore() {
+
+}
